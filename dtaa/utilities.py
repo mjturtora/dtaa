1607e819f1e89fa47efc2ccdb70823256ce97c1c
mjturtora <mjturtora@gmail.com>
2022-06-19 13:47:31 -0400

    Simple CLI

--- dtaa/utilities.py
+++ dtaa/utilities.py
@@ -1,6 +1,7 @@
 # This file is part of dtaa.
 # AUTHOR: Michael Turtora
 # 11/11/2018
+#
 
 # dtaa is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
